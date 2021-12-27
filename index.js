1d79dbcab1a45967632670c9b1231b5be8f9135b
openwell <timileyinojo@yahoo.com>
2021-12-27 07:03:29 +0100

    install cors and helmet

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
